00c108438c661ad9c4173865ac214dbd782cc060
Marcelo Fabri <marcelofabrimf@gmail.com>
2017-05-17 20:54:10 +0200

    Initial Commit

---
+++ BrokenFirebaseTests/BrokenFirebaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  BrokenFirebaseTests.swift
+//  BrokenFirebaseTests
+//
+//  Created by Marcelo Fabri on 17/05/17.
+//  Copyright © 2017 Marcelo Fabri. All rights reserved.
+//
+
+import XCTest
+@testable import BrokenFirebase
+
+class BrokenFirebaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
